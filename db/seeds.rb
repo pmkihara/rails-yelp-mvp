2d6153b05ee24ec40f1d2bfea58c6c0e7f3b5dd2
Patricia Kihara <patricia.kihara@outlook.com>
2021-08-18 16:22:37 -0300

    Restaurant list with working forms and bootstrap styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'creating 5 restaurants...'
+
+# Restaurant.create(
+#   [
+#     { name: 'China in Box', address: 'Aplicativos de comida ou telefone', category: 'chinese' },
+#     { name: "Nello's", address: 'Rua Antonio Bicudo', category: 'italian'},
+#     { name: 'Yashiro', address: 'Rua Fernão Dias', category: 'japanese'},
+#     { name: 'Meats', address: 'Rua dos Pinheiros', category: 'belga' }
+#   ]
+# )
+
+# puts '...finished!'
+
+# puts 'creating 1 restaurant...'
+# restaurant = Restaurant.new({ name: 'Meats', address: 'Rua dos Pinheiros', category: 'belgian' })
+# restaurant.save!
+# puts '...finished!'
